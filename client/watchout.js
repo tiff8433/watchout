2247472ecdae62b2f41db86cdfa8b85bd60af552
tiff8433 <tiffany.l.huang@gmail.com>
2015-12-04 16:49:08 -0800

    Adds enemies, moves randomly.

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,14 @@
 // start slingin' some d3 here.
+// function game(){
+  
+    d3.selectAll('.enemy').data([1,1,1,1,1,1,1,1,1,1])
+    .transition(1000)
+    .attr('cx',function (d) {return Math.floor(Math.random()*690);}) 
+    .attr('cy',function (d) {return Math.floor(Math.random()*440);})   
+    
+      // .attr('cx', function(d){ return Math.floor(Math.random()*700); })
 
+      // .attr('cy', function(d){ return Math.floor(Math.random()*450); });
+  
+// };
+// setTimeout(function(){ game() }, 1000);
